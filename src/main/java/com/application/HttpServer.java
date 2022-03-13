4bce13a00016dc5af82f5df6ab8dbac865dcecb3
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2022-03-13 14:18:05 +0300

    payloads of post requests are readable

--- src/main/java/com/application/HttpServer.java
+++ src/main/java/com/application/HttpServer.java
@@ -1,12 +1,16 @@
 package com.application;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class HttpServer {
-
+    private static final int SP = 0x20;//32 space
+    private static final int CR = 0x0D;//13 \r
+    private static final int LF = 0x0A;//10 \n
 
     public void start(int port) throws IOException {
         try (ServerSocket socket = new ServerSocket(port)) {
@@ -14,7 +18,6 @@ public class HttpServer {
                 try (Socket client = socket.accept()) {
                     handle(client);
                 }
-
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -24,17 +27,32 @@ public class HttpServer {
 
     public void handle(Socket socket) throws IOException {
         InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        StringBuilder requestBuilder = new StringBuilder();
+        BufferedReader br = new BufferedReader(inputStreamReader);
 
-        String line;
-        while ((line = bufferedReader.readLine()) != null && !"".equals(line)) {
-            requestBuilder.append(line + "\r\n");
+        //code to read and print headers
+        StringBuilder header = new StringBuilder();
+        String headerLine;
+        while (StringUtils.isNotBlank(headerLine = br.readLine())) {
+            System.out.println(headerLine);
         }
+        System.out.println(header);
 
+        //code to read the post payload data
+        StringBuilder payload = new StringBuilder();
+        while (br.ready()) {
+            char c = ((char) br.read());
+            if (c == CR) {
+                c = (char) br.read();
+                if (c == LF) {
+                }
+            } else {
+                if (StringUtils.isNotBlank(String.valueOf(c))) {
+                    payload.append(c);
+                }
+            }
+        }
+        System.out.println(payload);
 
-        String request = requestBuilder.toString();
-        System.out.println(request);
 
         OutputStream clientOutput = socket.getOutputStream();
         clientOutput.write(("HTTP/1.1 \r\n" + 500).getBytes());
@@ -45,11 +63,7 @@ public class HttpServer {
         clientOutput.flush();
         socket.close();
 
-       /* BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        bufferedWriter.append("It Works!");
-        bufferedWriter.append("...");
-        bufferedWriter.flush();
-        socket.close();*/
+
     }
 
 }
