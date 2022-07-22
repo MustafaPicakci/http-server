197285e99ca5a99bfcab7108cb4b2c594ae0124c
Mustafa <mustafapicakci25@gmail.com>
2022-07-22 22:25:59 +0300

    checkpoint

--- src/main/java/com/application/HttpTask.java
+++ src/main/java/com/application/HttpTask.java
@@ -11,19 +11,19 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class HttpTask implements Runnable {
     private final Socket client;
+    private final ObjectMapper objectMapper;
     private static final int SP = 0x20;//32 space
     private static final int CR = 0x0D;//13 \r
     private static final int LF = 0x0A;//10 \n
 
     public HttpTask(Socket client) {
         this.client = client;
+        this.objectMapper=new ObjectMapper();
     }
 
     @Override
@@ -36,9 +36,6 @@ public class HttpTask implements Runnable {
     }
 
     public void handle(Socket socket) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-
-
         HttpRequest httpRequest = new HttpRequest();
 
         InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
@@ -54,7 +51,6 @@ public class HttpTask implements Runnable {
                 httpRequest.getHeader().put(key, value);
             }
         }
-        System.out.println(httpRequest);
 
         //code to read the post payload data
         StringBuilder body = new StringBuilder();
@@ -71,19 +67,21 @@ public class HttpTask implements Runnable {
             }
         }
         if (body.length() > 0) {
-            String[] fields= String.valueOf(body).split(",");
-           Map<Object,Object> mapBody= mapper.readValue(String.valueOf(body), HashMap.class);
-
+            String[] fields = String.valueOf(body).split(",");
+            Map<Object, Object> mapBody = objectMapper.readValue(String.valueOf(body), HashMap.class);
+            System.out.println(mapBody);
         }
-        System.out.println(httpRequest);
+
         System.out.println(body);
 
         OutputStream clientOutput = socket.getOutputStream();
         clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-        clientOutput.write(("ContentType: text/html\r\n").getBytes());
-        clientOutput.write("\r\n".getBytes());
-        clientOutput.write("<b>It works!</b>".getBytes());
-        clientOutput.write("\r\n\r\n".getBytes());
+        if (StringUtils.isNotBlank(header.toString())) {
+            clientOutput.write(("ContentType: text/html\r\n").getBytes());
+            clientOutput.write("\r\n".getBytes());
+            clientOutput.write("<b>It works!</b>".getBytes());
+            clientOutput.write("\r\n\r\n".getBytes());
+        }
         clientOutput.flush();
         socket.close();
     }
